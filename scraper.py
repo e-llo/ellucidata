7e40685f26956f18d69b65a071adc1376c0c81b3
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-09-27 12:29:15 -0300

    get element's data

--- scraper.py
+++ scraper.py
@@ -6,6 +6,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+def extraiProduto(produto):
+	preco = produto.find_element_by_css_selector('div.sc-eCssSg.bcHdwl').text
+	descricao = produto.find_element_by_css_selector('div.sc-eCssSg.edSRJP').text
+	fabricante = produto.find_element_by_css_selector('div.sc-eCssSg.gVkgMB').text
+	return [descricao, fabricante, preco]
+
+
 driver = webdriver.Chrome()
 driver.get('https://www.supermercadonow.com/')
 
@@ -31,17 +39,24 @@ inputItem.send_keys(Keys.RETURN)
 
 
 secaoSupermercados = WebDriverWait(driver, 20).until(
-	EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[class="sc-jSgupP styles__Root-sc-1ltite1-0 cHuVIX gstWgI"]'))
+	EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.sc-jSgupP.styles__Root-sc-1ltite1-0.cHuVIX.gstWgI'))
 )
-lista_nomes = driver.find_elements_by_css_selector('div[class="sc-eCssSg inyvdf styles__Name-sc-1ltite1-4 doNrdT"]')
+lista_nomes = driver.find_elements_by_css_selector('div.sc-eCssSg.inyvdf.styles__Name-sc-1ltite1-4.doNrdT')
 
 
 dados = []
 i=0
 for secao in secaoSupermercados:
+	produto = extraiProduto(secao)
 	dados.append({
-		'supermercado': lista_nomes[i].text
+		'supermercado': lista_nomes[i].text,
+		'descricao': produto[0],
+		'fabricante': produto[1],
+		'preco': produto[2],
 	})
 	i+=1
 print(dados)
 driver.close()
+
+
+
