91cedfa3029e2ec65f17b63d035915a49f7f5f81
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-10-05 17:55:17 -0300

    formatting export data

---
+++ insert_data.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import scraper
+
+
+lista_produtos = [
+	['arroz', 'Arroz Branco Prato Fino Tipo 1 - 5kg'],
+	['extrato_tomate', 'Extrato de Tom Elefante Lata 130g'],
+	['acucar', 'Açúcar Refinado União 1 kg'],
+	['feijao', 'Feijão Preto Camil - 1Kg'],
+	['oleo_soja', 'Óleo de Soja Tipo 1 Liza Garrafa 900ml'],
+	['manteiga', 'Manteiga com Sal Aviação 200g'],
+	['pao', 'Pão de Forma Pullman - 480g'],
+	['cafe', 'Café Melitta Tradicional Vácuo 500g'],
+	['fuba', 'Fubá Mimoso Yoki Pacote 500g'],
+	['leite_condensado', 'Leite Condensado MOÇA Lata 395g'],
+	['macarrao', 'Macarrão com Ovos Espaguete 8 Barilla Pacote 500g'],
+	['creme_dental', 'Creme Dental Menta Original Colgate Tripla Ação Caixa 90g'],
+	['sabonete', 'Sabonete em Barra Antibacteriano Protex Cartucho 85g'], 
+	['sal', 'Sal Refinado Cisne - 1kg'],
+	['papel_higienico', 'Papel Higiênico NEVE Toque de Seda - 4 Rolos']
+]
+
+itens = {} # reúne todos os dados de todos os produtos em todos os supermercados
+
+for item in lista_produtos:
+    # Cria um atributo com o nome da primeira string do array como rotulo no dicionario "itens"
+    # e insere o resultado do metodo scraping usando a segunda string como parametro
+    itens[item[0]] = scraper.scraping(item[1])
+    print(item[0], "=>", itens[item[0]])
+    print("--------------------------------------------------")
+
+# Formata os dados coletados
+dados_formatados = {
+	"data" : datetime.now(),
+	"itens" : itens
+}
+
+
+scraper.driver.close()
--- scraper.py
+++ scraper.py
@@ -6,24 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-lista_produtos = [
-	'Arroz Branco Prato Fino Tipo 1 - 5kg',
-	'Extrato de Tom Elefante Lata 130g',
-	'Açúcar Refinado União 1 kg',
-	'Feijão Preto Camil - 1Kg',
-	'Óleo de Soja Tipo 1 Liza Garrafa 900ml',
-	'Manteiga com Sal Aviação 200g',
-	'Pão de Forma Pullman - 480g',
-	'Café Melitta Tradicional Vácuo 500g',
-	'Fubá Mimoso Yoki Pacote 500g',
-	'Leite Condensado MOÇA Lata 395g',
-	'Macarrão com Ovos Espaguete 8 Barilla Pacote 500g',
-	'Creme Dental Menta Original Colgate Tripla Ação Caixa 90g',
-	'Sabonete em Barra Antibacteriano Protex Cartucho 85g', 
-	'Sal Refinado Cisne - 1kg',
-	'Papel Higiênico NEVE Toque de Seda - 4 Rolos'
-]
-
 
 def extraiProduto(produto):
 	try:
@@ -64,7 +46,6 @@ def scraping(nome_produto):
 		})
 		i+=1
 	driver.get('https://www.supermercadonow.com/mercados') 
-	print(dados)
 	return dados
 
 
@@ -84,11 +65,9 @@ inputCEP.send_keys(Keys.RETURN)
 resumo_dados = [] # reúne todos os dados de todos os produtos em todos os supermercados
 
 # chamo a função de pesquisa e scraping dos dados
-for i in lista_produtos:
-	resumo_dados.append(scraping(i))	
-print(resumo_dados)
-
-driver.close()
+# for i in lista_produtos:
+# 	resumo_dados.append(scraping(i))	
+# print(resumo_dados)
 
 
 
