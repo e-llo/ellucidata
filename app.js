23b1d18b684b1ce8e7154b083f54f72250c7bf4e
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-10-24 16:08:09 -0300

    create web application structure for visualization

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const wrap = require("express-async-error-wrapper");
+const path = require("path");
+const ejs = require("ejs")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "/public"), {
+	cacheControl: false, // true
+	// etag: false,
+	// maxAge: "30d"
+}));
+
+// Configura o Express para reconhecer dados provenientes do corpo das requisições
+// http://expressjs.com/en/api.html#express.json
+// http://expressjs.com/en/api.html#express.urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define o view engine como o ejs e importa o express-ejs-layouts
+// https://www.npmjs.com/package/express-ejs-layouts, pois o ejs não
+// suporta layouts nativamente: https://www.npmjs.com/package/ejs#layouts
+app.set("view engine", "ejs");
+app.use(require("express-ejs-layouts"));
+app.use((req, res, next) => {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.header("Expires", "-1");
+    res.header("Pragma", "no-cache");
+    next();
+});
+
+app.get("/", wrap(async (req, res) => {
+    res.render("index");
+}));
+
+app.listen(1337, () => console.log("Executando servidor na porta 1337..."))
