23a30434f1e2a4bac94d820ea1b5322fdf84de1e
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-11-12 19:20:16 -0300

    getMin and getMax Functions

--- public/script.js
+++ public/script.js
@@ -1,10 +1,32 @@
-function getMax(produto, supermercado, mes){
-    // função puxa o valor máximo do produto no supermercado no mês
+
+function getMax(supermercado, produto){
+
+    let lista = dados.flatMap(dia => 
+        parseFloat(dia.supermercados[supermercado]
+            .filter(prod => prod.produto == produto)
+            .map(prod=> prod.preco.toString()
+                .replace("R$ ", "")
+                .replace(",",".")))
+    )
+    var max = Math.max(...lista);
+    return max
 }
-function getMin(produto, supermercado, mes){
+function getMin(produto, supermercado){
     // função puxa o valor mínimo do produto no supermercado no mês
+    let lista = dados.flatMap(dia => 
+        parseFloat(dia.supermercados[supermercado]
+            .filter(prod => prod.produto == produto)
+            .map(prod=> prod.preco.toString()
+                .replace("R$ ", "")
+                .replace(",",".")))
+    )
+    var max = Math.min(...lista);
+    return max
 }
 
 function variacaoMensal(produto, supermercado, mes){
-    getMax(produto, supermercado, mes);
+    // getMax(produto, supermercado, mes);
 }
+
+var array = getMax('extra_hiper', 'arroz')
+console.log(array)
