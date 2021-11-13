d4a222c231068511b97bc22cb719601d69899eed
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-11-13 18:54:41 -0300

    solving bugs on getMax()

--- public/script.js
+++ public/script.js
@@ -1,30 +1,52 @@
 
+
 function getMax(supermercado, produto){
 
     let lista = dados.flatMap(dia => 
-        parseFloat(dia.supermercados[supermercado]
+        parseFloat(dia.supermercados[supermercado] != undefined ? dia.supermercados[supermercado]
             .filter(prod => prod.produto == produto)
             .map(prod=> prod.preco.toString()
-                .replace("R$ ", "")
-                .replace(",",".")))
+            .replace("R$ ", "")
+            .replace(",",".")) :  null)
     )
+    // removing NaN from the list
+    var i = 0;
+    while (i < lista.length) {
+        if (Number.isNaN(lista[i])) {
+        lista.splice(i, 1);
+        } else {
+        ++i;
+        }
+    }
+
     var max = Math.max(...lista);
     return max
+   
 }
-function getMin(produto, supermercado){
+function getMin(supermercado,produto){
     // função puxa o valor mínimo do produto no supermercado no mês
     let lista = dados.flatMap(dia => 
-        parseFloat(dia.supermercados[supermercado]
+        parseFloat(dia.supermercados[supermercado] != undefined ? dia.supermercados[supermercado]
             .filter(prod => prod.produto == produto)
             .map(prod=> prod.preco.toString()
-                .replace("R$ ", "")
-                .replace(",",".")))
+            .replace("R$ ", "")
+            .replace(",",".")) :  null)
     )
-    var max = Math.min(...lista);
-    return max
+    
+    var i = 0;
+    while (i < lista.length) {
+        if (Number.isNaN(lista[i])) {
+        lista.splice(i, 1);
+        } else {
+        ++i;
+        }
+    }
+
+    var min = Math.min(...lista);
+    return min
 }
 
-function variacaoMensal(produto, supermercado, mes){
+function variacaoMensal(produto, supermercado){
     // getMax(produto, supermercado, mes);
 }
 
