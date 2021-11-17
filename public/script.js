be37db7998fcc7e9d05c307c0d52bbc1521aa216
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-11-17 18:46:19 -0300

    add table (product dash)

--- public/script.js
+++ public/script.js
@@ -46,7 +46,7 @@ function getMin(supermercado,produto){
 
 
 
-function variacaoSemanal(produto, supermercado){
+function maxMinSemanais(produto, supermercado){ // retorna porcentagem de variação máxima e mínima dentro da semana
     
     let lista = dados.flatMap(dia => 
         parseFloat(dia.supermercados[supermercado] != undefined ? dia.supermercados[supermercado]
@@ -68,7 +68,7 @@ function variacaoSemanal(produto, supermercado){
                     listaSemanalAux.push(lista[i])
                 }
             }
-            variacaoPercent = (Math.max(...listaSemanalAux) - Math.min(...listaSemanalAux))/Math.min(...listaSemanalAux)
+            var variacaoPercent = (Math.max(...listaSemanalAux) - Math.min(...listaSemanalAux))/Math.min(...listaSemanalAux)
             valoresSemanais.push((variacaoPercent*100).toFixed(1))
             j = j+7
         }else{
@@ -79,3 +79,126 @@ function variacaoSemanal(produto, supermercado){
     return valoresSemanais
 }
 
+
+
+function variacaoSemanal(produto, supermercado){ // retorna a variação semanal em porcentagem
+    
+
+    let lista = dados.flatMap(dia => 
+        parseFloat(dia.supermercados[supermercado] != undefined ? dia.supermercados[supermercado]
+            .filter(prod => prod.produto != undefined && prod.produto == produto)
+            .map(prod=> prod.preco.toString()
+            .replace("R$ ", "")
+            .replace(",",".")) :  null)
+    )
+    
+    // debugger;
+    var valoresSemanais = []
+    var j = 0
+    while(j<lista.length){
+        if(lista.length>=j+7){
+            var listaSemanalAux = []
+            for(var i = j; i<j+7;i++){
+                if (Number.isNaN(lista[i])) {
+                    listaSemanalAux.push(0)
+                }else{
+                    listaSemanalAux.push(lista[i])
+                }
+            }
+            var variacao = listaSemanalAux[6] - listaSemanalAux[0]
+            var percentual = variacao/listaSemanalAux[0]
+            valoresSemanais.push((percentual*100).toFixed(1))
+            j = j+7
+        }else{
+            j = lista.length
+        }
+
+    }
+    return valoresSemanais
+}
+
+function variacaoSemanalTodos(produto){ // retorna a variação semanal em porcentagem
+    
+
+    let dadosProduto = dados.filter(dia => dia.itens[produto] != undefined)
+    .map(dia => dia.itens[produto].map(obj => {return {data: dia.data, ...obj}}))
+
+    let dadosFinal = [];
+    let listaProdSupermercados = [float][float]
+    for(var i = 0; i<dadosProduto.length; i++){ //42 dias
+        for(var j = 0; j<dadosProduto[i].length; j++){ //10 supermercados
+            listaProdSupermercados[j][i] = dadosProduto[i]["preco"]
+        }
+    }
+
+
+
+    lista[0].mercados
+    .map(obj => obj.supermercado) // capturar o nome de cada mercado
+    .sort((a, b) => a.supermercado < b.supermercado ? -1:1) // ordem alfabetica
+    .forEach(mercado => {
+
+    let dadosMercados = {
+        datas: infos.map(mercadoAux => mercadoAux.data), // Datas
+        y: infos.map(mercadoAux => mercadoAux["preco"]), // Precos
+        name: conversorMercado(mercado).tela
+    }
+
+    dadosFinal.push(dadosMercados)        
+    })
+    
+    // debugger;
+    var valoresSemanais = []
+    var j = 0
+    while(j<lista.length){
+        if(lista.length>=j+7){
+            var listaSemanalAux = []
+            for(var i = j; i<j+7;i++){
+                if (Number.isNaN(lista[i])) {
+                    listaSemanalAux.push(0)
+                }else{
+                    listaSemanalAux.push(lista[i])
+                }
+            }
+            var variacao = listaSemanalAux[6] - listaSemanalAux[0]
+            var percentual = variacao/listaSemanalAux[0]
+            valoresSemanais.push((percentual*100).toFixed(1))
+            j = j+7
+        }else{
+            j = lista.length
+        }
+
+    }
+    return valoresSemanais
+}
+
+
+
+// let lista = dados.filter(dia => dia.itens[produto] != undefined)
+//             .map(dia => {
+//             return {
+//                 data: dia.data,
+//                 produto: produto,
+//                 mercados: dia.itens[produto]
+//                     .map(prod => {return {data: dia.data, ...prod}}) // Inserir data em cada mercado
+//             }
+//         })
+        
+//         let dadosFinal = [];
+
+//         lista[0].mercados
+//          .map(obj => obj.supermercado) // capturar o nome de cada mercado
+//          .sort((a, b) => a.supermercado < b.supermercado ? -1:1) // ordem alfabetica
+//          .forEach(mercado => {
+//             let infos = lista
+//                 .flatMap(dia => dia.mercados)
+//                 .filter(mercadoAux => mercadoAux.supermercado == mercado)
+            
+//             let dadosMercados = {
+//                 datas: infos.map(mercadoAux => mercadoAux.data), // Datas
+//                 y: infos.map(mercadoAux => mercadoAux["preco"]), // Precos
+//                 name: conversorMercado(mercado).tela
+//             }
+    
+//             dadosFinal.push(dadosMercados)        
+//         })
